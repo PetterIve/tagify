0fed77f358c2e2622abe2aa6f54ab9071e3e0cd9
petterive <iversen332@gmail.com>
2022-05-29 17:23:23 +0200

    add ui for adding tags

--- components/molecules/SpotifyTrack.tsx
+++ components/molecules/SpotifyTrack.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import { useState } from "react";
+import { Row } from "../atoms";
+import { Button } from "../atoms/Button";
+import { Input } from "../atoms/Input";
 
 interface Props {
   imageSrc: string;
@@ -7,13 +11,28 @@ interface Props {
 }
 
 export const SpotifyTrack = ({ imageSrc, trackName, artistName }: Props) => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [tagValue, setTagValue] = useState<string>("");
+
+  const submitTag = async () => {
+    setTags([...tags, tagValue]);
+    setTagValue("");
+  };
+
   return (
-    <div>
+    <Row>
       <Image src={imageSrc} alt="Track image" height={300} width={300} />
       <p>
         <b>{artistName}</b>
       </p>
       <p>{trackName}</p>
-    </div>
+      {tags.map((tag) => (
+        <p style={{ borderRadius: 1337, background: "pink" }} key={tag}>
+          {tag}
+        </p>
+      ))}
+      <Input value={tagValue} onValueChanged={setTagValue} label="Add tag" />
+      <Button text="Add tag" onClick={submitTag} />
+    </Row>
   );
 };
